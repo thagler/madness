f4d1418a4cef8318a1bf6efa3c19f4c3b2feb64f
Tobby Hagler <thagler@phase2technology.com>
2018-04-02 23:11:35 -0400

    Display a tabular list of users, sorted by madness level, in our block

--- src/Plugin/Block/TopMadness.php
+++ src/Plugin/Block/TopMadness.php
@@ -3,6 +3,7 @@
 namespace Drupal\madness\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\user\Entity\User;
 
 /**
  * Provides a block to display highest insane users.
@@ -19,8 +20,32 @@ class TopMadness extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+
+    // Query for user entities sorted by the madness_level field.
+    $query = \Drupal::entityQuery('user')
+      ->condition('status', 1)
+      ->condition('uid', 1, '>')
+      ->condition('madness_level', 0, '>')
+      ->sort('madness_level', 'DESC');
+
+    // Get User IDs.
+    $users = User::loadMultiple($query->execute());
+
+    // Load User entities and get the values we want to display.
+    $user_data = [];
+    foreach ($users as $uid => $user) {
+      $user_data[$uid] = [
+        'name' => $user->getDisplayName(),
+        'madness_level' => $user->get('madness_level')->value,
+      ];
+    }
+
+    // Return a tabular renderable array of user madness levels.
     return array(
-      '#markup' => $this->t('Most mad users!'),
+      '#type'   => 'table',
+      '#header' => ['User', 'Madness level'],
+      '#rows'   => $user_data,
+      '#empty'  => $this->t('No one is mad (yet!)'),
     );
   }
 
