d85fc3b8d802f9c70e2c5ff4e1abb01aff64caaf
Tobby Hagler <thagler@phase2technology.com>
2018-04-03 01:15:06 -0400

    Added a block-level configuration option to link to the user page or not

--- src/Plugin/Block/TopMadness.php
+++ src/Plugin/Block/TopMadness.php
@@ -6,6 +6,8 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\user\Entity\User;
 use Drupal\Core\Block\BlockPluginInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * Provides a block to display highest insane users.
@@ -18,12 +20,18 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class TopMadness extends BlockBase implements BlockPluginInterface {
 
+  // Set a default value to use for number of users to display.
+  private $user_count = 5;
+
+  // Set a default value for linking to user pages or not.
+  private $link = FALSE;
+
   /**
    * {@inheritdoc}
    */
   public function build() {
     $config = $this->getConfiguration();
-    $config['user_count'] = $config['user_count'] ?: 5;
+    $config['user_count'] = $config['user_count'] ?: $this->user_count;
 
     // Query for user entities sorted by the madness_level field.
     $query = \Drupal::entityQuery('user')
@@ -39,8 +47,12 @@ class TopMadness extends BlockBase implements BlockPluginInterface {
     // Load User entities and get the values we want to display.
     $user_data = [];
     foreach ($users as $uid => $user) {
+      // Link to the user page if the block is configured to do so.
+      $username = $config['link'] ? Link::fromTextAndUrl($user->getDisplayName(), Url::fromUri('internal:/user/' . $uid)) : $user->getDisplayName();
+
+      // Create the table row array values.
       $user_data[$uid] = [
-        'name' => $user->getDisplayName(),
+        'name' => $username,
         'madness_level' => $user->get('madness_level')->value,
       ];
     }
@@ -62,12 +74,20 @@ class TopMadness extends BlockBase implements BlockPluginInterface {
 
     $config = $this->getConfiguration();
 
+    $count = (int) $config['user_count'];
     $form['user_count'] = [
       '#type' => 'select',
       '#options' => [3 => 3, 5 => 5, 7 => 7, 10 => 10, 15 => 15],
       '#title' => $this->t('Number of users'),
       '#description' => $this->t('How many insane users should this block display?'),
-      '#default_value' => isset($config['user_count']) ? $config['user_count'] : '',
+      '#default_value' => $count ?: $this->user_count,
+    ];
+
+    $form['link'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Link to user?'),
+      '#description' => $this->t('Should the user\'s name in the block link to their user page (if users have permission to view them)?'),
+      '#default_value' => $config['link'] ?: $this->link,
     ];
 
     return $form;
@@ -80,6 +100,7 @@ class TopMadness extends BlockBase implements BlockPluginInterface {
     parent::blockSubmit($form, $form_state);
     $values = $form_state->getValues();
     $this->configuration['user_count'] = $values['user_count'];
+    $this->configuration['link'] = $values['link'];
   }
 
 }
